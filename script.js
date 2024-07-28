e709fb8c625570a59be4a048fec14497e48d1c86
steve rogers <35471257-mapletay1220@users.noreply.replit.com>
2024-07-28 15:46:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function setup(){
+  createCanvas(1200, 550);
+  background(150, 200, 250);
+}
+
+
+function myfunction(){
+
+  
+  var x = 1024; //min value
+  var y = 9999; // max value
+
+  var deg = Math.floor(Math.random() * (x - y)) + y;
+
+  document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+
+  var element = document.getElementById('mainbox');
+  element.classList.remove('animate');
+  setTimeout(function(){
+    element.classList.add('animate');
+  }, 5000); //5000 = 5 second
+}
+
